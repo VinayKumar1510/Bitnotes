0ede1b5ff0fd843fce38331f83e6edacc78807d6
VinayKumar1510 <146341986+VinayKumar1510@users.noreply.github.com>
2025-04-11 21:22:16 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import Typed from "typed.js";
 import React, { useRef, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Home() {
 
@@ -24,49 +23,40 @@ export default function Home() {
 
   return (
     <main>
-      <section className="px-6 py-20 mx-auto lg:space-x-8 lg:flex lg:items-center flex-col lg:flex-row">
-        <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
+      <section className="px-6 py-20 mx-auto flex flex-col items-center text-center">
+        <div className="w-full lg:w-2/3">
           <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
             A{" "}
             <span className="font-semibold text-blue-500">free application</span>{" "}
-            for you <br className="hidden lg:block" />
+            for you <br />
             to make{" "}
             <span className="underline decoration-primary text-pink-600">
               <span ref={el} />
             </span>
           </h1>
           <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-            This is a free application in which you can make/store <br className="hidden lg:block" />
+            This is a free application in which you can make/store <br />
             your important{" "}
             <span className="underline decoration-primary text-fuchsia-500 text-2xl">
               Notes and Todos
             </span>
           </p>
-          <div className="mt-6 bg-transparent lg:w-2/3 focus-within:border-primary justify-center">
+          <div className="mt-6 flex flex-col sm:flex-row justify-center gap-4">
             <Link href="/makenow">
-              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+              <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
                   Make a Note
                 </span>
               </button>
             </Link>
             <Link href="/record">
-              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+              <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
                   Record Now
                 </span>
               </button>
             </Link>
           </div>
-        </div>
-        <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
-          <Image
-          width={100}
-          height={100}
-            src="/main.png"
-            alt="an image for main page"
-            className="w-full h-full max-w-md mx-auto"
-          />
         </div>
       </section>
 
